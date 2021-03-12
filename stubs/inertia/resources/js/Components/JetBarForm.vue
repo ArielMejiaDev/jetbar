8557ce581108e3f0ccf40630040d4b853f189937
ArielMejiaDev <arielmejiadev@gmail.com>
2021-03-11 18:51:32 -0600

    Adding form components support

---
+++ stubs/inertia/resources/js/Components/JetBarForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <jet-section-title class="my-4">
+            <template #title><slot name="title"></slot></template>
+            <template #description><slot name="description"></slot></template>
+        </jet-section-title>
+
+        <div class="mt-5 md:mt-0 md:col-span-2">
+            <form @submit.prevent="$emit('submitted')">
+                <div class="px-4 py-5 bg-white sm:p-6 shadow"
+                     :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
+                    <div>
+                        <slot name="form"></slot>
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md" v-if="hasActions">
+                    <slot name="actions"></slot>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import JetSectionTitle from '@/Jetstream/SectionTitle';
+
+export default {
+    name: "JetBarForm",
+
+    emits: ['submitted'],
+
+    components: {
+        JetSectionTitle,
+    },
+
+    computed: {
+        hasActions() {
+            return !! this.$slots.actions
+        }
+    }
+}
+</script>
---
+++ stubs/inertia/resources/js/Components/JetBarInput.vue
@@ -0,0 +1,41 @@
+<template>
+    <jet-label :for="name" :value="name" />
+
+    <jet-input ref="input" v-bind="$attrs" class="w-full mt-1 block" :id="name" :type="type" :ref="name" @input="$emit('input', $event.target.value)" />
+
+    <jet-input-error class="mt-2" :message="form.errors[name]" />
+</template>
+
+<script>
+import JetLabel from "@/Jetstream/Label";
+import JetInput from "@/Jetstream/Input";
+import JetInputError from "@/Jetstream/InputError";
+
+export default {
+    name: "JetBarInput",
+    components: {
+        JetLabel,
+        JetInput,
+        JetInputError,
+    },
+    props: {
+        name: String,
+        type: {
+            type: String,
+            default: 'text',
+        },
+        form: Object,
+    },
+    methods: {
+        focus() {
+            this.$refs.input.focus()
+        },
+        select() {
+            this.$refs.input.select()
+        },
+        setSelectionRange(start, end) {
+            this.$refs.input.setSelectionRange(start, end)
+        },
+    },
+}
+</script>
